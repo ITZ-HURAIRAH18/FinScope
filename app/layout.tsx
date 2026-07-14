bec5ad0ea219e248d2efc0107d08e0b03cbedb83
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2026-07-14 09:38:30 +0500

    Add Vercel Analysis Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Providers } from "./providers";
 import Footer from "@/components/Footer";
@@ -29,6 +30,7 @@ export default function RootLayout({
           <CleanupTrigger />
           {children}
           <Footer />
+          <Analytics />
         </Providers>
       </body>
     </html>
